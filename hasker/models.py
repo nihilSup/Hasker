b7096acb650758b5296165bf68822ec2dd72f0ea
GoodInc <igor.s.stulikov@gmail.com>
2019-08-26 18:53:23 +0300

    added custom user model

--- hasker/models.py
+++ hasker/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class HaskerUser(AbstractUser):
+
+    avatar = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return super().username
