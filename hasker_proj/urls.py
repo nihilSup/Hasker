d742fca76fe5b12ffb79c2d9b21b1c909de91f33
GoodInc <igor.s.stulikov@gmail.com>
2019-08-26 14:18:32 +0300

    added mininal functionality

--- hasker_proj/urls.py
+++ hasker_proj/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+
+from hasker import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'), 
+    url(r'^hasker/', include('hasker.urls')),
 ]
