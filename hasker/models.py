d4693a71903d768f319ea3feb832d75ed8002726
GoodInc <igor.s.stulikov@gmail.com>
2019-08-26 20:17:32 +0300

    added basic models

--- hasker/models.py
+++ hasker/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+
 
 class HaskerUser(AbstractUser):
 
@@ -7,3 +9,29 @@ class HaskerUser(AbstractUser):
 
     def __str__(self):
         return super().username
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    author = models.ForeignKey(HaskerUser, on_delete=models.CASCADE)
+    asked_date = models.DateTimeField(default=timezone.now)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(HaskerUser, on_delete=models.CASCADE)
+    answered_date = models.DateTimeField(default=timezone.now)
+    is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
