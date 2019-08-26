d04245e1b45180a5604ef05384cbe4ce4abed851
GoodInc <igor.s.stulikov@gmail.com>
2019-08-26 23:30:07 +0300

    added support for static content

--- hasker_proj/settings.py
+++ hasker_proj/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 
 # Custom user model
 AUTH_USER_MODEL = 'hasker.HaskerUser'
