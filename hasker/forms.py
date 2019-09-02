1ab91cdfb42b11afdeba5ee5d791342cfda2977a
GoodInc <igor.s.stulikov@gmail.com>
2019-09-02 16:32:27 +0300

    refactored forms

--- hasker/forms.py
+++ hasker/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
-from .models import HaskerUser, Question
+from .models import HaskerUser, Question, Tag
 
 
 class HaskerUserCreationForm(UserCreationForm):
@@ -19,14 +19,7 @@ class HaskerUserChangeForm(UserChangeForm):
         fields = '__all__'
 
 
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    password_re = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = get_user_model()
-        fields = ('username', 'password', 'password_re', 'email', 'avatar')
-
+class BootsModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
@@ -34,6 +27,15 @@ class UserForm(forms.ModelForm):
                 'class': 'form-control',
                 'placeholder': field.label or ''
             })
+
+
+class UserForm(BootsModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_re = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password', 'password_re', 'email', 'avatar')
 
 
 class UserProfileForm(forms.ModelForm):
@@ -46,15 +48,16 @@ class UserProfileForm(forms.ModelForm):
         self.fields['username'].widget.attrs['readonly'] = True
 
 
-class QuestionForm(forms.ModelForm):
+class QuestionForm(BootsModelForm):
     class Meta:
         model = Question
         fields = ('title', 'content', 'tags')
 
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all())
+
     def __init__(self, *args, **kwargs):
+        if kwargs.get('instance'):
+            initial = kwargs.setdefault('initial', {})
+            initial['tags'] = [
+                tag.pk for tag in kwargs['instance'].tag_set.all()]
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs.update({
-                'class': 'form-control',
-                'placeholder': field.label or ''
-            })
