6fc6566c5f98c456554d16ccbc568912acebd322
GoodInc <igor.s.stulikov@gmail.com>
2019-09-10 21:25:11 +0300

    added static debug handling

--- hasker_proj/hasker_proj/urls.py
+++ hasker_proj/hasker_proj/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^hasker/', include('hasker.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
