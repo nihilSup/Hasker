e58c880f50cb7c714e3a03e54df0f70558ee228b
GoodInc <igor.s.stulikov@gmail.com>
2019-09-11 17:57:41 +0300

    added secret key handling

--- hasker_proj/hasker_proj/settings.py
+++ hasker_proj/hasker_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&q2@4%v$6^5po@xnh+c4g62oqsan3jwoitj+)k573!(lje3i8p'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&q2@4%v$6^5po@xnh+c4g62oqsan3jwoitj+)k573!(lje3i8p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.getenv('DJANGO_ENV') == 'prod':
