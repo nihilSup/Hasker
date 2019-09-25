757301d7a1c11414201f7464ec125e703461c4e5
GoodInc <igor.s.stulikov@gmail.com>
2019-09-25 22:49:46 +0300

    replaced url with path method

--- hasker_proj/hasker_proj/urls.py
+++ hasker_proj/hasker_proj/urls.py
@@ -8,10 +8,10 @@ from hasker import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
-    url(r'^hasker/', include('hasker.urls')),
+    path('', views.index, name='index'),
+    path('hasker/', include('hasker.urls')),
 ] 
 
-if bool(settings.DEBUG):
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
