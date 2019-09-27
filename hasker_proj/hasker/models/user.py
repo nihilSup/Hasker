f5cf4c5a549054338f71dc9a474ae3b6baf5c394
GoodInc <igor.s.stulikov@gmail.com>
2019-09-27 15:43:33 +0300

    added function to get image url with default value

--- hasker_proj/hasker/models/user.py
+++ hasker_proj/hasker/models/user.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.templatetags.static import static
 
 
 class HaskerUser(AbstractUser):
 
     avatar = models.ImageField(upload_to='profile_images', blank=True)
+
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        else:
+            return static("hasker/images/default.png")
 
     def __str__(self):
         return super().username
